1bedf6f5bcb94f7348b26979323b0103f13c9ed7
Harika-0304 <143828915+Harika-0304@users.noreply.github.com>
2024-01-08 11:57:03 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(page_title='Cats')
+st.header("Types of Cats")
+
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Persian Cat")
+  st.image("./Persian.jpg",caption="Persian Cat",width=300,use_column_width=True)
+  st.write("Persian Cats are Cute")
+
+with col2:
+  st.subheader("Rangdoll Cat")
+  st.image("./rangdoll.jpg",caption="Rangdoll Cat",width=300,use_column_width=True)
+  st.write("Rangdoll Cats are proud")
